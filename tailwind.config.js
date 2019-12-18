9e04733956da3d13de4bf60e0f2be297a6a0ffda
kalzpi <48274440+kalzpi@users.noreply.github.com>
2019-12-18 17:02:18 +0900

    Tailwind, Gulp, Gitignore, SCSS, SASS setup done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        "72": "18rem",
+        "84": "21rem",
+        "96": "24rem",
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      },
+      borderRadius: {
+        xl: "1.5rem"
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
